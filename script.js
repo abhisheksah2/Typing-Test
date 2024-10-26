1556ab0f2d305dd144e5788cba5fe823dd1cb326
abhisheksah2 <135296031+abhisheksah2@users.noreply.github.com>
2024-10-26 20:53:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //Random Quotes Api URL
-const quoteApiUrl = "Those who know do not speak; those who speak do not know";
+const quoteApiUrl = "";
 const quoteSection = document.getElementById("quote");
 const userInput = document.getElementById("quote-input");
-let quote = "";https://api.quotable.io/random?minLength=80&maxLength=100
+let quote = "";//https:api.quotable.io/random?minLength=80&maxLength=100
 let time = 60;
 let timer = "";
 let mistakes = 0;
