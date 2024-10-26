144ddf35c9c3ef0aa3bb7361cbb6f1f01235aa87
abhisheksah2 <135296031+abhisheksah2@users.noreply.github.com>
2024-10-26 20:56:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Random Quotes Api URL
-const quoteApiUrl = "";
+const quoteApiUrl = "https://api.quotable.io/random?minLength=80&maxLength=100";
 const quoteSection = document.getElementById("quote");
 const userInput = document.getElementById("quote-input");
 let quote = "";
